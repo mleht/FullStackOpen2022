9be324aed6831afcdda991eae571e8165d5cc724
Mikko Lehtinen <[]>
2022-07-04 21:33:39 +0300

    4.15

--- osa4/app.js
+++ osa4/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 require('express-async-errors')
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
 
