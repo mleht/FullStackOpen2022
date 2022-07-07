a7a7b076a5ec6093a5c339925690d8af8eaec200
Mikko Lehtinen <[]>
2022-07-07 13:29:24 +0300

    4.19

--- osa4/utils/logger.js
+++ osa4/utils/logger.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
+  if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
   }
 }
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
+  if (process.env.NODE_ENV !== 'test') {
     console.error(...params)
   }
 }
