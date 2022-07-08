40c0c829b87d64aa9971e5a1577e0cddbf137f21
Mikko Lehtinen <[]>
2022-07-08 20:53:36 +0300

    5.2

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -18,6 +18,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -25,6 +34,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      ) 
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -36,6 +50,16 @@ const App = () => {
     }
   }
 
+  const logout = () => {
+    localStorage.clear();
+    setUser(null);
+  
+    setErrorMessage("You have been successfully logged out");
+  
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 4000);
+  };
 
   if (user === null) {
     return (
@@ -71,7 +95,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={logout}>Logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
