cacf16e1dfddf883c639d0616d664c3c4642f344
Mikko Lehtinen <[]>
2022-07-11 21:28:46 +0300

    5.7

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    padding: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+  <div style={blogStyle}>
+    {!showDetails &&
+    <>
+    {blog.title} {blog.author} <button onClick={() => setShowDetails(!showDetails)}>View details</button>
+    </>
+    }
+    
+    {showDetails && 
+    <>
+    {blog.title} {blog.author} <button onClick={() => setShowDetails(!showDetails)}>Hide details</button>
+    <br/>
+    {blog.url}
+    <br/>
+    likes {blog.likes} <button>Like</button>
+    <br/>
+    {blog.user.name}
+    </>
+    }
   </div>  
-)
+)}
 
 export default Blog
+
+
+
+
+
+
+
+
