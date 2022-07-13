61f7f7a1b9b6e297465b278b9532be974b819e5d
Mikko Lehtinen <[]>
2022-07-13 15:06:33 +0300

    5.14

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 
 test('renders content', () => {
@@ -16,11 +17,35 @@ test('renders content', () => {
     <Blog blog={blog} />
   )
 
-  screen.debug(component.container)
+  // screen.debug(component.container)
 
   expect(component.container).toHaveTextContent('test title')
   expect(component.container).toHaveTextContent('test author')
   expect(component.container).not.toHaveTextContent('www.test.net')
   expect(component.container).not.toHaveTextContent(13)
+
+})
+
+test('after clicking the View details button, url & likes are displayed', async () => {
+  const blog = {
+    title: 'test title',
+    author: 'test author',
+    url: 'www.test.net',
+    likes: 13,
+    user: { name: 'testUser' }
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View details')
+  await user.click(button)
+
+  screen.debug(component.container)
+
+  expect(component.container).toHaveTextContent('www.test.net')
+  expect(component.container).toHaveTextContent(13)
 
 })
