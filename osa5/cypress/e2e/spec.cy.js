2864ecad1a5a5848ec2d3ce4d76dea5351cfcf23
Mikko Lehtinen <[]>
2022-07-15 18:00:15 +0300

    5.18

--- osa5/cypress/e2e/spec.cy.js
+++ osa5/cypress/e2e/spec.cy.js
@@ -1,6 +1,19 @@
 describe('Blog app', function() {
+  /*
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+  */
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Testi Testinen',
+      username: 'testi',
+      password: 'testi'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,4 +23,25 @@ describe('Blog app', function() {
     cy.contains('password')
     cy.contains('login')
   })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testi')
+      cy.get('#password').type('testi')
+      cy.get('#login-button').click()
+
+      cy.contains('Testi Testinen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testi')
+      cy.get('#password').type('testi2')
+      cy.get('#login-button').click()
+
+      cy.get('.neg')
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
+
 })
