99994f940cdb73dc896a2e1ab77290bba83a10bc
Mikko Lehtinen <[]>
2022-07-15 19:14:37 +0300

    5.21

--- osa5/cypress/e2e/spec.cy.js
+++ osa5/cypress/e2e/spec.cy.js
@@ -79,6 +79,22 @@ describe('Blog app', function() {
         .and('have.css', 'color', 'rgb(0, 128, 0)')
       cy.contains('likes 1')
     })
+
+    it('User who created a blog can delete it.', function() {
+      cy.contains('New blog').click()
+      cy.get('input[placeholder="title"]').type('test_title')
+      cy.get('input[placeholder="author"]').type('test_author')
+      cy.get('input[placeholder="url"]').type('test_url')
+      cy.get('#create-button').click()
+      cy.get('.pos')
+
+      cy.contains('View details').click()
+      cy.contains('Delete').click()
+      cy.on('window:confirm', () => true)
+      cy.get('.pos')
+        .should('contain', 'deleted')
+        .and('have.css', 'color', 'rgb(0, 128, 0)')
+    })
   })
 
 })
