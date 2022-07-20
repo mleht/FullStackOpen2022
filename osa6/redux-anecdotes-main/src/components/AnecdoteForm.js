ca25d5e7d26a4df2d9d4ede8f86e79a5d2fac563
Mikko Lehtinen <[]>
2022-07-20 12:51:26 +0300

    6.20

--- osa6/redux-anecdotes-main/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes-main/src/components/AnecdoteForm.js
@@ -1,6 +1,43 @@
-import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteSliceReducer'
 import { notification } from '../reducers/notificationReducer'
+import { connect } from 'react-redux' 
+
+const AnecdoteForm = (props) => {
+  const addNew = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value     // content tulee returnin formista nimeltään anecdote
+    event.target.anecdote.value = ''
+    props.createAnecdote(content)
+    props.notification(`You created '${content}'`, 5)
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addNew}>
+        <div><input name="anecdote" /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+const mapDispatchToProps = {                          
+  createAnecdote,
+  notification
+}
+
+const ConnectedAnecdoteForm = connect(        // connect-funktion toisena parametrina voidaan määritellä mapDispatchToProps eli joukko action creator -funktioita, jotka välitetään yhdistetylle komponentille propseina
+  null,                                       // Koska komponentti ei tarvitse storen tilasta mitään, on connect-funktion ensimmäinen parametri null.
+  mapDispatchToProps
+)(AnecdoteForm)
+
+export default ConnectedAnecdoteForm
+
+
+/*
+
+Alla Redux-storea käytetään  useDispatch-hookin avulla. Yllä taas connect-funktionlla
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -25,3 +62,5 @@ const AnecdoteForm = () => {
 }
 
 export default AnecdoteForm
+
+*/
--- osa6/redux-anecdotes-main/src/components/Filter.js
+++ osa6/redux-anecdotes-main/src/components/Filter.js
@@ -1,3 +1,37 @@
+import { connect } from 'react-redux' 
+import { searchFilter } from '../reducers/filterReducer'
+
+const Filter = (props) => {
+  const handleChange = (event) => {
+    props.searchFilter(event.target.value)
+  }
+
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+const mapDispatchToProps = {
+  searchFilter                          // importoitu filterReducerista ja välitetään propsina Filter-komponentille
+}
+
+const ConnectedFilter = connect(        // connect-funktion toisena parametrina voidaan määritellä mapDispatchToProps eli joukko action creator -funktioita, jotka välitetään yhdistetylle komponentille propseina
+  null,                                 // Koska komponentti ei tarvitse storen tilasta mitään, on connect-funktion ensimmäinen parametri null.
+  mapDispatchToProps
+)(Filter)
+
+export default ConnectedFilter
+
+/*
+
+Alla Redux-storea käytetään  useSelector-hookin avulla. Yllä taas connect-funktionlla
+
 import { useDispatch } from "react-redux";  
 import { searchFilter } from "../reducers/filterReducer";
 
@@ -20,3 +54,5 @@ const Filter = () => {
 }
 
 export default Filter
+
+*/
