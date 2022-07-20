5f69c8576ec144207b00b6cc0973d3d183d85f40
Mikko Lehtinen <[]>
2022-07-20 11:46:12 +0300

    6.19

--- osa6/redux-anecdotes-main/src/components/Notification.js
+++ osa6/redux-anecdotes-main/src/components/Notification.js
@@ -1,3 +1,32 @@
+import { connect } from 'react-redux' 
+
+const Notification = (props) => {
+  const notification = props.notification_connect_test
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  return (
+    <>
+    {notification? 
+      <div style={style}> {notification} </div> : '' }
+    </>
+  )
+}
+
+const mapStateToProps = (state) => {                          //   This function selects parts of the Redux state and passes it in as props to the component that connect() is applied to
+  return {                                                    //   Eli tässä tapauksessa Notification saa propsina notificationRecuderin staten
+    notification_connect_test: state.notificationReducer
+  }
+}
+
+export default connect(mapStateToProps)(Notification)
+
+/*
+
+Alla Redux-storea käytetään  useSelector-hookin avulla. Yllä taas connect-funktionlla
+
 import { useSelector } from 'react-redux'
 
 const Notification = () => {
@@ -16,3 +45,5 @@ const Notification = () => {
 }
 
 export default Notification
+
+*/
