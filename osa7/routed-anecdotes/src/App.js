74547bc840f2892673c55603c2a35249c882f705
Mikko Lehtinen <[]>
2022-07-27 19:19:24 +0300

    7.5

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -83,6 +83,13 @@ const CreateNew = (props) => {
     navigate('/')   
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -99,7 +106,7 @@ const CreateNew = (props) => {
           url for more info
           <input  {...info} /> 
         </div>
-        <button>create</button>
+        <button>create</button> <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
